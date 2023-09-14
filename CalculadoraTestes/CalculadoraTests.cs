8813e6c0511c010713f17fe87217e5af2bb97635
Brayancrc <brayancrc@gmail.com>
2023-09-14 16:02:19 -0300

    first commit

---
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,87 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+    
+    [Fact]
+    public void DeveSomar5com10ERetornar15()
+    {
+        //Arange
+        int num1 = 5;
+        int num2 = 10;
+
+        //Act
+        int resultado = _calc.Somar(num1, num2);
+
+        //Assert
+        Assert.Equal(15, resultado);
+
+    }
+
+      [Fact]
+    public void DeveSomar10com10ERetornar20()
+    {
+        //Arange
+        int num1 = 10;
+        int num2 = 10;
+
+        //Act
+        int resultado = _calc.Somar(num1, num2);
+
+        //Assert
+        Assert.Equal(20, resultado);
+
+    }
+
+    [Fact]
+    public void DeveVerificarSe4EhParERetornarVerdadeiro()
+    {
+        //Arange
+        int num = 4;
+        
+
+        //Act
+        bool resultado = _calc.EhPar(num);
+
+        //Assert
+        Assert.True(resultado);
+
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    public void DeveVerificarSeOsNumerosSaoParERetornarVerdadeiro(int numero)
+    {
+        // Given
+    
+        // When
+        bool resultado = _calc.EhPar(numero);
+    
+        // Then
+        Assert.True(resultado);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 2, 4 })]
+    [InlineData(new int[] { 6, 8, 10 })]
+    public void DeveVerificarSeOsNumerosDoArraySaoParERetornarVerdadeiro(int[] numeros)
+    {
+        // Given
+        // When  
+        // Then
+        Assert.All(numeros, num => Assert.True(_calc.EhPar(num)));
+    }
+
+}
